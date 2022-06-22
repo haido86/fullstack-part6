6647b683cf86b1336c2388e4563a0f8e22e1b49a
haido86 <thithuhai.do@gmail.com>
2022-06-22 06:45:01 +0300

    6.13 Anecdotes and the backend, step1

---
+++ redux-anecdotes/src/services/anecdotes.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const baseUrl = 'http://localhost:3001/anecdotes';
+
+const getAll = async () => {
+  const response = await axios.get(baseUrl);
+  //   console.log('response.data', response.data);
+  return response.data;
+};
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { getAll };

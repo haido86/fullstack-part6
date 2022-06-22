b8764ddaa70857b7feda8664ace31de30494aa9d
haido86 <thithuhai.do@gmail.com>
2022-06-22 10:19:55 +0300

    6.15 Anecdotes and the backend, step3

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import anecdoteService from '../services/anecdotes';
 
 const anecdoteSlice = createSlice({
   name: 'anecdote',
@@ -15,10 +16,7 @@ const anecdoteSlice = createSlice({
         anecdote.id !== id ? anecdote : votedAnecdote
       );
     },
-    createAnecdote(state, action) {
-      const content = action.payload;
-      state.push(content);
-    },
+
     appendAnecdote(state, action) {
       state.push(action.payload);
     },
@@ -28,6 +26,20 @@ const anecdoteSlice = createSlice({
   },
 });
 
-export const { addVote, createAnecdote, appendAnecdote, setAnecdotes } =
-  anecdoteSlice.actions;
+export const { addVote, appendAnecdote, setAnecdotes } = anecdoteSlice.actions;
+
+export const initializeAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anecdoteService.getAll();
+    dispatch(setAnecdotes(anecdotes));
+  };
+};
+
+export const createAnecdote = (content) => {
+  return async (dispatch) => {
+    const newAnecdote = await anecdoteService.createNew(content);
+    dispatch(appendAnecdote(newAnecdote));
+  };
+};
+
 export default anecdoteSlice.reducer;
